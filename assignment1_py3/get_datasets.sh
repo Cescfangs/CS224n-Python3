b1aad3b39ca53a623fa5f06d15f4d376bf45851b
Cescfangs <cescfangs@gmail.com>
2018-02-18 17:34:41 +0800

    first commit

---
+++ assignment1_py3/get_datasets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DATASETS_DIR="utils/datasets"
+mkdir -p $DATASETS_DIR
+
+cd $DATASETS_DIR
+
+# Get Stanford Sentiment Treebank
+if hash wget 2>/dev/null; then
+  wget http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+else
+  curl -O http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+fi
+unzip stanfordSentimentTreebank.zip
+rm stanfordSentimentTreebank.zip
+
+# Get 50D GloVe vectors
+# if hash wget 2>/dev/null; then
+#   wget https://nlp.stanford.edu/data/glove.6B.zip
+# else
+#   curl -O https://nlp.stanford.edu/data/glove.6B.zip
+# fi
+# unzip glove.6B.zip
+# rm glove.6B.zip
